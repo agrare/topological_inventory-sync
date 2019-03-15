d4e0bbff8bf30713843d691332b51ce0e99af90c
Adam Grare <agrare@redhat.com>
2019-03-15 10:42:43 -0400

    Push faktory job when work comes in

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 plugin "bundler-inject", "~> 1.1"
 require File.join(Bundler::Plugin.index.load_paths("bundler-inject")[0], "bundler-inject") rescue nil
 
+gem "faktory_worker_ruby"
 gem "manageiq-loggers", "~> 0.1.0"
 gem "manageiq-messaging"
 gem "optimist"
--- lib/topological_inventory/sync/worker.rb
+++ lib/topological_inventory/sync/worker.rb
@@ -1,4 +1,5 @@
 require "manageiq-messaging"
+require "faktory"
 require "topological_inventory/sync/logging"
 
 module TopologicalInventory
@@ -7,6 +8,7 @@ module TopologicalInventory
       include Logging
 
       def initialize(messaging_host, messaging_port, queue_name)
+        self.faktory_client   = Faktory::Client.new
         self.messaging_host   = messaging_host
         self.messaging_port   = messaging_port
         self.queue_name       = queue_name
@@ -16,9 +18,7 @@ module TopologicalInventory
         logger.info("Starting Topological Inventory Sync Worker for #{queue_name}...")
 
         messaging_client = ManageIQ::Messaging::Client.open(messaging_client_opts)
-        messaging_client.subscribe_messages(subscribe_opts) do |messages|
-          messages.each { |message |process_message(message) }
-        end
+        messaging_client.subscribe_topic(subscribe_opts) { |message| process_message(message) }
       rescue => err
         logger.error(err.message)
         logger.error(err.backtrace.join("\n"))
@@ -28,9 +28,18 @@ module TopologicalInventory
 
       private
 
-      attr_accessor :messaging_host, :messaging_port, :queue_name
+      attr_accessor :faktory_client, :messaging_host, :messaging_port, :queue_name
 
       def process_message(message)
+        faktory_client.push(
+          "jid"     => SecureRandom.hex(12),
+          "queue"   => "default",
+          "jobtype" => queue_name,
+          "args"    => [
+            message.message,
+            message.payload
+          ]
+        )
       rescue => err
         logger.error(err.message)
         logger.error(err.backtrace.join("\n"))
@@ -38,15 +47,18 @@ module TopologicalInventory
 
       def messaging_client_opts
         {
-          :protocol => :Kafka,
-          :host     => messaging_host,
-          :port     => messaging_port
+          :protocol   => :Kafka,
+          :host       => messaging_host,
+          :port       => messaging_port,
+          :group_ref  => "topological-inventory-sync-#{queue_name}",
+          :client_ref => "topological-inventory-sync-#{queue_name}"
         }
       end
 
       def subscribe_opts
         {
-          :service => queue_name,
+          :service     => queue_name,
+          :persist_ref => "topological-inventory-sync"
         }
       end
     end
