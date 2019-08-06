0884d87ee42146b72de1a9f4d0f75b9803ca8c3c
Adam Grare <agrare@redhat.com>
2019-08-06 15:35:11 -0400

    Add CFME Payload spec

---
+++ spec/topological_inventory/sync/inventory_upload/payload/cfme_spec.rb
@@ -0,0 +1,17 @@
+require "topological_inventory/sync/inventory_upload/payload"
+require_relative "../../../../helpers/inventory_upload_helper"
+
+RSpec.describe TopologicalInventory::Sync::InventoryUpload::Payload::Cfme do
+  include InventoryUploadHelper
+
+  context "#process" do
+    it "parses cfme inventory and sends to ingress-api" do
+      payload = described_class.new(cfme_inventory, "12345")
+
+      expect(payload).to receive(:find_or_create_source)
+      expect(payload).to receive(:send_to_ingress_api)
+
+      payload.process
+    end
+  end
+end
