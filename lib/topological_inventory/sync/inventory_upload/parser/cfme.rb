1ffc4a29c6c5dd4e32a898dcb053a7b58cdf5106
Adam Grare <agrare@redhat.com>
2019-08-07 15:50:15 -0400

    Split parsing up into different methods

--- lib/topological_inventory/sync/inventory_upload/parser/cfme.rb
+++ lib/topological_inventory/sync/inventory_upload/parser/cfme.rb
@@ -8,50 +8,73 @@ module TopologicalInventory
 
             clusters_collection = ingress_api_inventory_collection("clusters")
             payload["ems_clusters"].to_a.each do |cluster_data|
-              clusters_collection.data << TopologicalInventoryIngressApiClient::Cluster.new(
-                :name       => cluster_data["name"],
-                :source_ref => cluster_data["ems_ref"],
-                :uid_ems    => cluster_data["uid_ems"],
-                :extra      => {
-                  :ha_enabled       => cluster_data["ha_enabled"],
-                  :drs_enabled      => cluster_data["drs_enabled"],
-                  :effective_cpu    => cluster_data["effective_cpu"],
-                  :effective_memory => cluster_data["effective_memory"]
-                }
-              )
+              clusters_collection.data << parse_cluster(cluster_data)
             end
             inventory.collections << clusters_collection
 
             hosts_collection = ingress_api_inventory_collection("hosts")
-
             payload["hosts"].to_a.each do |host_data|
-              memory_mb = host_data.dig("hardware", "memory_mb")
-              cluster_ref = host_data.dig("ems_cluster", "ems_ref")
-              cluster     = ingress_api_lazy_ref("clusters", cluster_ref) unless cluster_ref.nil?
-
-              hosts_collection.data << TopologicalInventoryIngressApiClient::Host.new(
-                :name        => host_data["name"],
-                :hostname    => host_data["hostname"],
-                :ipaddress   => host_data["ipaddress"],
-                :power_state => host_data["power_state"],
-                :uid_ems     => host_data["uid_ems"],
-                :source_ref  => host_data["ems_ref"],
-                :cpus        => host_data["cpu_total_cores"],
-                :memory      => memory_mb * 1048576,
-                :cluster     => cluster,
-                :extra       => {
-                  :cpu_cores_per_socket => host_data["cpu_cores_per_socket"],
-                  :maintenance          => host_data["maintenance"],
-                  :vmm_vendor           => host_data["vmm_vendor"],
-                  :vmm_version          => host_data["vmm_version"],
-                  :vmm_product          => host_data["vmm_product"],
-                  :vmm_buildnumber      => host_data["vmm_buildnumber"],
-                }
-              )
+              hosts_collection.data << parse_host(host_data)
             end
-
             inventory.collections << hosts_collection
 
+            datastores_collection, datastore_mounts_collection = parse_storages(payload)
+
+            inventory.collections << datastores_collection
+            inventory.collections << datastore_mounts_collection
+
+            vms_collection = ingress_api_inventory_collection("vms")
+            payload["vms"].to_a.each do |vm_data|
+              vms_collection.data << parse_vm(vm_data)
+            end
+            inventory.collections << vms_collection
+
+            inventory
+          end
+
+          private
+
+          def parse_cluster(cluster_data)
+            TopologicalInventoryIngressApiClient::Cluster.new(
+              :name       => cluster_data["name"],
+              :source_ref => cluster_data["ems_ref"],
+              :uid_ems    => cluster_data["uid_ems"],
+              :extra      => {
+                :ha_enabled       => cluster_data["ha_enabled"],
+                :drs_enabled      => cluster_data["drs_enabled"],
+                :effective_cpu    => cluster_data["effective_cpu"],
+                :effective_memory => cluster_data["effective_memory"]
+              }
+            )
+          end
+
+          def parse_host(host_data)
+            memory_mb = host_data.dig("hardware", "memory_mb")
+            cluster_ref = host_data.dig("ems_cluster", "ems_ref")
+            cluster     = ingress_api_lazy_ref("clusters", cluster_ref) unless cluster_ref.nil?
+
+            TopologicalInventoryIngressApiClient::Host.new(
+              :name        => host_data["name"],
+              :hostname    => host_data["hostname"],
+              :ipaddress   => host_data["ipaddress"],
+              :power_state => host_data["power_state"],
+              :uid_ems     => host_data["uid_ems"],
+              :source_ref  => host_data["ems_ref"],
+              :cpus        => host_data["cpu_total_cores"],
+              :memory      => memory_mb * 1048576,
+              :cluster     => cluster,
+              :extra       => {
+                :cpu_cores_per_socket => host_data["cpu_cores_per_socket"],
+                :maintenance          => host_data["maintenance"],
+                :vmm_vendor           => host_data["vmm_vendor"],
+                :vmm_version          => host_data["vmm_version"],
+                :vmm_product          => host_data["vmm_product"],
+                :vmm_buildnumber      => host_data["vmm_buildnumber"],
+              }
+            )
+          end
+
+          def parse_storages(payload)
             datastores_collection       = ingress_api_inventory_collection("datastores")
             datastore_mounts_collection = ingress_api_inventory_collection( "datastore_mounts")
 
@@ -84,50 +107,29 @@ module TopologicalInventory
               end
             end
 
-            inventory.collections << datastores_collection
-            inventory.collections << datastore_mounts_collection
-
-            vms_collection = ingress_api_inventory_collection("vms")
-            payload["vms"].to_a.each do |vm_data|
-              host_ref = vm_data.dig("host", "ems_ref")
-              host     = ingress_api_lazy_ref("hosts", host_ref) unless host_ref.nil?
-
-              vms_collection.data << TopologicalInventoryIngressApiClient::Vm.new(
-                :name        => vm_data["name"],
-                :description => vm_data["description"],
-                :cpus        => vm_data["cpu_total_cores"],
-                :memory      => vm_data["ram_size_in_bytes"],
-                :source_ref  => vm_data["ems_ref"],
-                :uid_ems     => vm_data["uid_ems"],
-                :power_state => vm_data["power_state"],
-                :host        => host,
-                :extra       => {
-                  :cpu_cores_per_socket => vm_data["cpu_cores_per_socket"],
-                  :disks_aligned        => vm_data["disks_aligned"],
-                  :has_rdm_disk         => vm_data["has_rdm_disk"],
-                  :linked_clone         => vm_data["linked_clone"],
-                  :retired              => vm_data["retired"],
-                  :v_datastore_path     => vm_data["v_datastore_path"],
-                }
-              )
-            end
-            inventory.collections << vms_collection
-
-            inventory
+            return datastores_collection, datastore_mounts_collection
           end
 
-          private
+          def parse_vm(vm_data)
+            host_ref = vm_data.dig("host", "ems_ref")
+            host     = ingress_api_lazy_ref("hosts", host_ref) unless host_ref.nil?
 
-          def parse_cluster(cluster_data)
-            TopologicalInventoryIngressApiClient::Cluster.new(
-              :name       => cluster_data["name"],
-              :source_ref => cluster_data["ems_ref"],
-              :uid_ems    => cluster_data["uid_ems"],
-              :extra      => {
-                :ha_enabled       => cluster_data["ha_enabled"],
-                :drs_enabled      => cluster_data["drs_enabled"],
-                :effective_cpu    => cluster_data["effective_cpu"],
-                :effective_memory => cluster_data["effective_memory"]
+            TopologicalInventoryIngressApiClient::Vm.new(
+              :name        => vm_data["name"],
+              :description => vm_data["description"],
+              :cpus        => vm_data["cpu_total_cores"],
+              :memory      => vm_data["ram_size_in_bytes"],
+              :source_ref  => vm_data["ems_ref"],
+              :uid_ems     => vm_data["uid_ems"],
+              :power_state => vm_data["power_state"],
+              :host        => host,
+              :extra       => {
+                :cpu_cores_per_socket => vm_data["cpu_cores_per_socket"],
+                :disks_aligned        => vm_data["disks_aligned"],
+                :has_rdm_disk         => vm_data["has_rdm_disk"],
+                :linked_clone         => vm_data["linked_clone"],
+                :retired              => vm_data["retired"],
+                :v_datastore_path     => vm_data["v_datastore_path"],
               }
             )
           end
