96b317774cab13a860884cd72dae75c464339a04
Libor Pichler <lpichler@redhat.com>
2019-09-16 19:50:41 +0200

    Refactor specs in cfme_specs.rb

--- spec/topological_inventory/sync/inventory_upload/payload/cfme_spec.rb
+++ spec/topological_inventory/sync/inventory_upload/payload/cfme_spec.rb
@@ -5,12 +5,12 @@ require 'topological_inventory-api-client'
 RSpec.describe TopologicalInventory::Sync::InventoryUpload::Payload::Cfme do
   include InventoryUploadHelper
 
-  context "#process" do
+  describe '#process' do
     let(:source) { SourcesApiClient::Source.new }
     let(:application) { SourcesApiClient::Application.new }
+    let(:payload) { described_class.new(cfme_inventory, '12345') }
 
-    it "parses cfme inventory and sends to ingress-api" do
-      payload = described_class.new(cfme_inventory, "12345")
+    it 'parses cfme inventory and sends to ingress-api' do
 
       payload.send(:cfme_ems_types).each do |ems_type|
         cfme_inventory[ems_type].to_a.each do |provider_payload|
@@ -25,40 +25,42 @@ RSpec.describe TopologicalInventory::Sync::InventoryUpload::Payload::Cfme do
       payload.process
     end
 
-    it 'parses cfme inventory and sends to ingress-api when source exists in topological inventory' do
-      payload = described_class.new(cfme_inventory, '12345')
-      api_client = payload.send(:topological_api_client)
+    context 'check if source exists in topology inventory' do
+      let(:api_client) { payload.send(:topological_api_client) }
+      let(:source_in_topology_inventory) { source }
 
-      payload.send(:cfme_ems_types).each do |ems_type|
-        cfme_inventory[ems_type].to_a.each do |provider_payload|
-          allow(payload).to receive(:find_source).with(api_client, provider_payload['guid']).and_return(source)
+      before do
+        payload.send(:cfme_ems_types).each do |ems_type|
+          cfme_inventory[ems_type].to_a.each do |provider_payload|
+            guid = provider_payload['guid']
+            allow(payload).to receive(:find_source).with(api_client, guid).and_return(source_in_topology_inventory)
+          end
         end
       end
 
-      expect(payload).to receive(:find_or_create_source).and_return(source)
-      expect(payload).to receive(:find_or_create_application).and_return(application)
-      expect(payload).to receive(:send_to_ingress_api)
+      it 'parses cfme inventory and sends to ingress-api when source exists in topological inventory' do
+        expect(payload).to receive(:find_or_create_source).and_return(source)
+        expect(payload).to receive(:find_or_create_application).and_return(application)
+        expect(payload).to receive(:send_to_ingress_api)
 
-      payload.process
-    end
-
-    it "doesn't send inventory to ingress-api when source doesn't exists in topological inventory yet" do
-      described_class::TIMEOUT_COUNT = 0.001.seconds.freeze
-      payload = described_class.new(cfme_inventory, '12345')
-      api_client = payload.send(:topological_api_client)
-
-      payload.send(:cfme_ems_types).each do |ems_type|
-        cfme_inventory[ems_type].to_a.each do |provider_payload|
-          guid = provider_payload['guid']
-          allow(payload).to receive(:find_source).with(api_client, guid).and_return(nil)
-        end
+        payload.process
       end
 
-      expect(payload).to receive(:find_or_create_source).and_return(source)
-      expect(payload).to receive(:find_or_create_application).and_return(application)
-      expect(payload).not_to receive(:send_to_ingress_api)
+      context "source doesn't exist in topological inventory yet" do
+        let(:source_in_topology_inventory) { nil }
 
-      payload.process
+        before do
+          described_class::TIMEOUT_COUNT = 0.001.seconds.freeze
+        end
+
+        it "doesn't send inventory to ingress-api" do
+          expect(payload).to receive(:find_or_create_source).and_return(source)
+          expect(payload).to receive(:find_or_create_application).and_return(application)
+          expect(payload).not_to receive(:send_to_ingress_api)
+
+          payload.process
+        end
+      end
     end
   end
 end
